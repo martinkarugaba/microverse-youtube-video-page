3f2401fec88701c95542ace968f3c4cc0c853203
Martin Karugaba <martinkarugaba21@gmail.com>
2022-11-22 14:11:58 +0300

    added flex positioning to nav-bar items

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363"
+      }
+    },
   },
   plugins: [],
-}
+};
+  
